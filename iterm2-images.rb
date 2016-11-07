676ee1f376901233bed84cbd86d0442177170f81
DT <dtodd@mitre.org>
2016-11-07 09:33:24 -0500

    update to 3.0.12 to reflect Oct. 1 imgcat updates

--- iterm2-images.rb
+++ iterm2-images.rb
@@ -1,9 +1,9 @@
 class Iterm2Images < Formula
   desc "Inline images for iTerm2"
   homepage "http://iterm2.com/documentation-images.html"
-  url "https://github.com/dmofot/iterm2-images/archive/2.0.1.tar.gz"
-  version "2.0.1"
-  sha256 "2afe6c13a61749253046cb985878b10ab77d16e0255ed9056547c83b2de75f44"
+  url "https://github.com/dmofot/iterm2-images/archive/3.0.12.tar.gz"
+  version "3.0.12"
+  sha256 "3948f9602434137b02780745fdd371e4e1a98c9da39fdb1d11c2c2ae02881b19"
 
   def install
     bin.install "imgcat"
